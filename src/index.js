8f93fd02a9eb40ecaa35ce00e0654747b9b05e88
pabloescampur <codeclone02@gmail.com>
2025-05-16 13:45:09 +0200

    uptime upload

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import UptimeDashboard from './UptimeDashboard';
+import './index.css';  // import style, bisa untuk Tailwind
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<UptimeDashboard />);
